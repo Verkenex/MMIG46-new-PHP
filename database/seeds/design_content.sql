b8f71eaa30d54a08e21d705582c56c2ace9fa44f
Max Gerecht <max@flatmail.de>
2026-06-15 11:35:08 +0200

    Fix dynamic image paths

--- database/seeds/design_content.sql
+++ database/seeds/design_content.sql
@@ -2,17 +2,17 @@ SET NAMES utf8mb4;
 
 UPDATE news_items
 SET
+  category = 'Verein',
+  image_path = '/assets/img/news-meeting.jpg',
+  comment_count = 0
+WHERE slug = 'neue-website-in-vorbereitung';
+
+UPDATE news_items
+SET
   category = 'Reisen',
   image_path = '/assets/img/news-woerthersee.jpg',
   comment_count = 0
 WHERE slug = 'fly-in-woerthersee-2026';
-
-UPDATE news_items
-SET
-  category = 'Verein',
-  image_path = '/assets/img/hero-pa46.jpg',
-  comment_count = 0
-WHERE slug = 'neue-website-in-vorbereitung';
 
 UPDATE travel_items
 SET
@@ -23,5 +23,5 @@ WHERE slug = 'fly-in-woerthersee-2026';
 UPDATE travel_items
 SET
   image_path = '/assets/img/travel-woerthersee.jpg',
-  cta_label = 'Rueckblick ansehen'
+  cta_label = 'Rückblick ansehen'
 WHERE slug = 'fly-in-woerthersee-2025';
