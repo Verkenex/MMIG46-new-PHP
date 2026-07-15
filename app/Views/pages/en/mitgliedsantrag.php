bd03a58fbd5badf2e3e544b5d1784f31bed1f0c5
Max Gerecht <max@flatmail.de>
2026-07-15 12:10:55 +0200

    LIVE!!!!! FIrst fixes

--- app/Views/pages/en/mitgliedsantrag.php
+++ app/Views/pages/en/mitgliedsantrag.php
@@ -13,9 +13,11 @@ $aircraftModels = [
     'PA46-310',
     'PA46-350',
     'PA46R-350T',
-    'PA46-500',
-    'PA46-JETPROP',
-    'Other',
+    'PA46-JetPROP',
+    'PA46-M500',
+    'PA46-M600',
+    'PA46-M700',
+    $isEn ? 'Other' : 'Andere',
 ];
 
 ?>
